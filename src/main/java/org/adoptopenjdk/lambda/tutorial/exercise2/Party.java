e774fbe08158de005bbc855aa1bcd113bf73a3aa
Grundlefleck <grundlefleck@gmail.com>
2013-04-21 12:37:57 +0100

    Add another filter example.

---
+++ src/main/java/org/adoptopenjdk/lambda/tutorial/exercise2/Party.java
@@ -0,0 +1,14 @@
+package org.adoptopenjdk.lambda.tutorial.exercise2;
+
+/**
+ * Lambda Tutorial -- Adopt Open JDK
+ *
+ * @author Graham Allan grundlefleck at gmail dot com
+ */
+public enum Party {
+    CONSERVATIVE,
+    LABOUR,
+    LIBERAL_DEMOCRATS,
+    GREEN_PARTY,
+    MONSTER_RAVING_LOONY_PARTY
+}
