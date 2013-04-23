026679959209c4aafb8a6bbf702f6a30d44f9308
Graham Allan <graham.allan@timgroup.com>
2013-04-23 22:04:42 +0100

    Add lambda solutions for exercise 2.

--- src/main/java/org/adoptopenjdk/lambda/tutorial/exercise2/ElectoralDistrict.java
+++ src/main/java/org/adoptopenjdk/lambda/tutorial/exercise2/ElectoralDistrict.java
@@ -1,21 +1,9 @@
 package org.adoptopenjdk.lambda.tutorial.exercise2;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.DelegatingStream;
-import java.util.stream.Stream;
 
 /**
  * Lambda Tutorial -- Adopt Open JDK
@@ -39,15 +27,18 @@ public enum ElectoralDistrict {
     }
 
     public static Set<RegisteredVoter> votersIn(ElectoralDistrict district, Collection<RegisteredVoter> voters) {
-        // [your code here]
+        Set<RegisteredVoter> fromDistrict = voters.stream()
+                .filter(v -> v.electorId.startsWith(district.prefix))
+                .collect(Collectors.toSet());
 
-        return Collections.emptySet();
+        return Collections.unmodifiableSet(fromDistrict);
     }
 
     public static Set<Ballot> unspoiledBallots(Set<Ballot> votes) {
-        // [your code here]
+        return votes.stream()
+                .filter(v -> !v.isSpoiled)
+                .collect(Collectors.toSet());
 
-        return Collections.emptySet();
     }
 }
 
