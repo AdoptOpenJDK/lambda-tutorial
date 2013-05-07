46b3eee2f99b6923f6a865e23701c90899a33662
Graham Allan <graham.allan@timgroup.com>
2013-05-07 22:40:57 +0100

    Make tests pass for JDK 8.

--- src/main/java/org/adoptopenjdk/lambda/tutorial/exercise3/Books.java
+++ src/main/java/org/adoptopenjdk/lambda/tutorial/exercise3/Books.java
@@ -3,23 +3,19 @@ package org.adoptopenjdk.lambda.tutorial.exercise3;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Books {
-    public static List<String> titlesOf(List<Book> books) {
-        // [your code here]
 
-        return Collections.emptyList();
+    public static List<String> titlesOf(List<Book> books) {
+        return books.stream().map(b -> b.title).collect(Collectors.toList());
     }
 
     public static List<String> namesOfAuthorsOf(List<Book> books) {
-        // [your code here]
-
-        return Collections.emptyList();
+        return books.stream().map(b -> b.author).map(Author::fullName).collect(Collectors.toList());
     }
 
     public static Set<Publisher> publishersRepresentedBy(List<Book> books) {
-        // [your code here]
-
-        return Collections.emptySet();
+        return books.stream().map(b -> b.publisher).collect(Collectors.toSet());
     }
 }
