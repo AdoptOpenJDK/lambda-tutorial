c19462754654fef3efa143753eb134d9e39b14cd
Graham Allan <graham.allan@timgroup.com>
2013-10-04 00:56:49 +0100

    Remove main method already removed in master.

--- src/main/java/org/adoptopenjdk/lambda/tutorial/exercise4/Printers.java
+++ src/main/java/org/adoptopenjdk/lambda/tutorial/exercise4/Printers.java
@@ -20,19 +20,4 @@ public class Printers {
         return contents.map(Page::new);
     }
 
-
-    public static void main(String... args) {
-        Page p1 = new Page("this is the first page");
-        Page p2 = new Page("this is the second page");
-
-        Document myDocument = new Document(Arrays.asList(p1, p2));
-
-        Printers.printPages(myDocument, 0, 1);
-
-        List<Page> pages = Arrays.asList(p1, p2);
-        pages.stream().map(Page::getContent).forEach(Printers::print);
-
-        Stream<Page> pagesFromContent = createPagesFrom(Arrays.asList("a", "b").stream());
-        System.out.println(pagesFromContent.collect(toList()));
-    }
 }
